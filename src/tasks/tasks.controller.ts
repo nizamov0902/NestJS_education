8700e786f97e05d5459049cebcf581fe71f0d036
Ruslan Nizamov <nizamoff.rus@gmail.com>
2023-01-17 13:55:49 +0100

    add getTaskById method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import {Task} from "./task.module";
 import {CreateTaskDto} from "./dto/create-task.dto";
@@ -12,9 +12,15 @@ export class TasksController {
         return this.tasksService.getAllTasks();
     }
 
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
+
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
       return this.tasksService.createTask(createTaskDto);
     }
+
 }
 
