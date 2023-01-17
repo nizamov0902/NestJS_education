9daaba45bcd9b35ddeca553a224a3930c42a3467
Ruslan Nizamov <nizamoff.rus@gmail.com>
2023-01-17 13:03:34 +0100

    add Get & Post requests, add create-task.dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
 
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
 })
 export class AppModule {}
+
+
