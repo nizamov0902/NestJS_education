e379b17725f6484baa698fef39bc43dedefbe877
Ruslan Nizamov <nizamoff.rus@gmail.com>
2023-01-20 16:11:21 +0100

    add error handling for dublicate username

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,6 +2,7 @@ import {EntityRepository, Repository} from "typeorm";
 import {User} from "./user.entity";
 import {AuthCredentialsDto} from "./dto/auth-credentials.dto";
 import {TaskStatus} from "../tasks/task.status.enum";
+import {ConflictException, InternalServerErrorException} from "@nestjs/common";
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -13,6 +14,14 @@ export class UsersRepository extends Repository<User> {
             password,
         });
 
-        await this.save(user);
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === '23505') { //duplicate username
+                throw new ConflictException('Username already exists')
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
