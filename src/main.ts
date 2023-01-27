c22ec85087d061e7dc61f6b14cffbeb5c9e2ba6c
Ruslan Nizamov <nizamoff.rus@gmail.com>
2023-01-27 13:52:52 +0100

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ValidationPipe} from "@nestjs/common";
-import { TransformInterceptor } from "./transform.interceptor";
-import { Logger } from "@nestjs/common";
+import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   const port = 3000;
