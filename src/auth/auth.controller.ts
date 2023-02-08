21d4f362c01901fbc16433e41b6f52b165bcf3ca
nizamov0902 <nizamoff.rus@gmail.com>
2023-02-08 18:02:04 +0100

    refactor signup method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,10 +10,10 @@ export class AuthController {
     return this.authService.signUp(authCredentialsDto);
   }
 
-  @Post('/signin')
-  signIn(
-    @Body() authCredentialsDto: AuthCredentialsDto,
-  ): Promise<{ accessToken: string }> {
-    return this.authService.signIn(authCredentialsDto);
-  }
+  //@Post('/signin')
+  //signIn(
+  //  @Body() authCredentialsDto: AuthCredentialsDto,
+  //): Promise<{ accessToken: string }> {
+  //  return this.authService.signIn(authCredentialsDto);
+  //}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,31 +5,37 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './jwt-payload.interface';
+import { UserService } from '../user.service';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(UsersRepository)
-    private usersRepository: UsersRepository,
+    //@InjectRepository(UsersRepository)
+    //private usersRepository: UsersRepository,
+    private userService: UserService,
     private jwtService: JwtService,
   ) {}
 
+  // async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  //return this.usersRepository.createUser(authCredentialsDto);
+  //}
+
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-    return this.usersRepository.createUser(authCredentialsDto);
+    return this.userService.createUser(authCredentialsDto);
   }
 
-  async signIn(
-    authCredentialsDto: AuthCredentialsDto,
-  ): Promise<{ accessToken: string }> {
-    const { username, password } = authCredentialsDto;
-    const user = await this.usersRepository.findOne({ username });
+  //async signIn(
+  //  authCredentialsDto: AuthCredentialsDto,
+  //): Promise<{ accessToken: string }> {
+  //  const { username, password } = authCredentialsDto;
+  //  const user = await this.usersRepository.findOne({ username });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const payload: JwtPayload = { username };
-      const accessToken: string = await this.jwtService.sign(payload);
-      return { accessToken };
-    } else {
-      throw new UnauthorizedException('Please check your login credentials');
-    }
-  }
+  //  if (user && (await bcrypt.compare(password, user.password))) {
+  //    const payload: JwtPayload = { username };
+  //    const accessToken: string = await this.jwtService.sign(payload);
+  //    return { accessToken };
+  //  } else {
+  //    throw new UnauthorizedException('Please check your login credentials');
+  //  }
+  //}
 }
